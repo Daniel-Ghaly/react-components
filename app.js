5b7d892cf0471d801ce7b2b9a5205b2930993f62
Daniel-Ghaly <ghalydan@gmail.com>
2021-11-29 12:34:01 -0800

    complete

--- app.js
+++ app.js
@@ -1,1 +1,77 @@
 // TODO
+// var App = () => (
+//   <div>Some cliche salutation</div>
+// );
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+var App = (props) => (
+
+  <GroceryList items = {['apple','banana','orange']} />
+
+
+);
+
+
+
+var GroceryList = (props) => (
+  <ul>
+  {props.items.map( (item) =>
+   <GroceryListItem item = {item}/>
+  )}
+</ul>
+)
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    )
+
+
+}
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+
+
+// props.items.forEach(item, function() {
+//   <li>{item}</li>
+// })
+
+
+
+
+
+
+
+
+
+
+
